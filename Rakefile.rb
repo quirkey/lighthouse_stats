724168dca70c88c5e446795322ecff4e631c3dac
Aaron Quint <aaron@quirkey.com>
2008-11-11 00:13:57 -0500

    Fixing dump - actually works - cool!

--- Rakefile.rb
+++ Rakefile.rb
@@ -3,16 +3,19 @@ require 'lighthouse_stats'
 namespace :lighthouse_stats do
   task :load_current do
     Lighthouse.account = ENV['ACCOUNT'] || 'intersect'
-    Lighthouse.token   = ENV['TOKEN'] || raise 'Supply an account token with TOKEN='
+    Lighthouse.token   = ENV['TOKEN'] || raise('Supply an account token with TOKEN=')
     @lighthouse = LighthouseStats.new
     @lighthouse.load
   end
   
   task :dump => :load_current do
-    path = ENV['PATH'] || File.expand_path('~/.lighthouse_stats/')
+    path = ENV['TO_PATH'] || File.expand_path('~/.lighthouse_stats/')
     FileUtils.mkdir_p(path)
     filename = "#{Lighthouse.account}_dump_#{Time.now.strftime('%Y-%m-%d')}.dump"
-    File.open(File.join(path, filename), 'w') {|f| f << Marshal.dump(f) }
+    full_path = File.join(path, filename)
+    puts "Marshaling to '#{full_path}'"
+    File.open(full_path, 'w') {|f| f << Marshal.dump(@lighthouse) }
+    puts "Wrote #{File.size(full_path) / 1024}k"
   end
   
 end
